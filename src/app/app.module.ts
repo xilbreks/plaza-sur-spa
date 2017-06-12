29892f3cffaef8f1848de8962e4b3d14ff3b63d4
socks <Xil.Breks@gmail.com>
2017-06-11 20:28:58 -0500

    borrando app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,8 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 import { ShellComponent } from './core/shell/shell.component';
 
-//import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
-  //  AppComponent
   ],
   imports: [
     BrowserModule,
